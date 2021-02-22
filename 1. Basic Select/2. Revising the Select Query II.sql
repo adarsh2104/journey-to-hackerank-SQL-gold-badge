25f9cb5d565a710add2a49d2df495d2abbeb45d2
Adarsh Sethi <adarsh.2104@gmail.com>
2021-02-22 20:25:41 +0530

    Added the solution to problem 2 for basic select query challenges

---
+++ 1. Basic Select/2. Revising the Select Query II.sql
@@ -0,0 +1,11 @@
+-- Github    : https://github.com/adarsh2104
+-- HR-Profile: https://www.hackerrank.com/adarsh_2104
+-- Challenge : https://www.hackerrank.com/challenges/revising-the-select-query-2
+-- Max Score : 10
+
+
+SELECT NAME FROM CITY 
+WHERE 
+    POPULATION > 120000 
+    AND 
+    COUNTRYCODE = "USA" ;
