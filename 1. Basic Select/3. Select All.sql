5cf2742d55b1d58d395d7f80e3827eca391d9182
Adarsh Sethi <adarsh.2104@gmail.com>
2021-02-23 19:34:35 +0530

    Added the solution to problem 3 for basic select query challenges

---
+++ 1. Basic Select/3. Select All.sql
@@ -0,0 +1,9 @@
+-- Github    : https://github.com/adarsh2104
+-- HR-Profile: https://www.hackerrank.com/adarsh_2104
+-- Challenge : https://www.hackerrank.com/challenges/select-all-sql
+-- Max Score : 10
+
+ 
+SELECT * FROM CITY 
+WHERE 
+    1 ;
