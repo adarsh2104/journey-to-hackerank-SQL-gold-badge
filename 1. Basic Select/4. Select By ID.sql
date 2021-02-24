309c5ad4bdc5902acb68b39588ab62377409a503
Adarsh Sethi <adarsh.2104@gmail.com>
2021-02-24 19:55:26 +0530

    Added the query for Problem 4 of Basic Select subdomain

---
+++ 1. Basic Select/4. Select By ID.sql
@@ -0,0 +1,9 @@
+-- Github    : https://github.com/adarsh2104
+-- HR-Profile: https://www.hackerrank.com/adarsh_2104
+-- Challenge : https://www.hackerrank.com/challenges/select-by-id
+-- Max Score : 10
+
+ 
+SELECT * FROM CITY 
+WHERE 
+     ID = 1661 ;
